1ae8781b345ca7a2c35adaf52abba1edd82ac3f5
Andy Boomer <aboomer07@gmail.com>
2020-11-16 22:01:03 -0800

    adjusted US section

--- Code/DataFetch.py
+++ Code/DataFetch.py
@@ -20,7 +20,8 @@ df = pd.read_csv(url, sep=',')
 df = df.drop(['Lat', 'Long'], axis=1)
 
 # wide to long
-df = df.melt(id_vars=['Province/State', 'Country/Region'], var_name='date', value_name='confirmed')
+df = df.melt(id_vars=['Province/State', 'Country/Region'],
+             var_name='date', value_name='confirmed')
 df['date'] = pd.to_datetime(df['date'], format="%m/%d/%y")
 
 # restrict to countries of interest (here US is grouped?)
@@ -33,16 +34,23 @@ df.to_csv(data_dir + 'EuropeCovidData.csv', sep=';', index=False)
 # A. US separate to get county level
 url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
 df_us = pd.read_csv(url, sep=',')
-df_us = df_us.drop(['Lat', 'Long_', 'UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Country_Region', 'Combined_Key'], axis=1).reset_index(drop=True)
-# check if UUID or FIPS are useful
+df_us = df_us.drop(['Lat', 'Long_', 'UID', 'iso2', 'iso3', 'code3',
+                    'Country_Region', 'Combined_Key', 'Admin2'], axis=1).reset_index(drop=True)
+df_us = df_us.rename({'Province_State': "State"}, axis=1)
+df_us['FIPS'] = df_us['FIPS'].apply(
+    lambda x: str(int(x)) if pd.notnull(x) else "NaN")
+df_us['FIPS'] = df_us['FIPS'].apply(
+    lambda x: "0" + x if len(x) == 4 else x)
+# FIPS is the county code, keeping instead of Admin2 which is county name
 
 # wide to long
-df_us = df_us.melt(id_vars=['Admin2', 'Province_State'], var_name='date', value_name='confirmed')
+df_us = df_us.melt(id_vars=['FIPS', 'State'],
+                   var_name='date', value_name='confirmed')
 df_us['date'] = pd.to_datetime(df_us['date'], format="%m/%d/%y")
 
 # restrict to counties of interest, US specific
-county_list = ['Oregon']
-df_us = df_us[df_us['Province_State'].isin(county_list)]
+# county_list = ['Oregon']
+# df_us = df_us[df_us['State'].isin(county_list)]
 
 # save data
 df_us.to_csv(data_dir + 'USCovidData.csv', sep=';', index=False)
