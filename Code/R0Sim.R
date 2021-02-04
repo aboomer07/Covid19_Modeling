9a27453a07fd6888e09ee5d24e7c22737e78693b
jacob_314 <jacob.pichelmann@gmail.com>
2021-02-05 00:32:19 +0100

    New R0 Estimation Graph based on actual data.

--- Code/R0Sim.R
+++ Code/R0Sim.R
@@ -10,8 +10,15 @@ library(tidyverse)
 imppath <- paste0(getwd(), '/Data/')
 outpath <- paste0(getwd(), '/Output/')
 
-sim_const <- read.csv2(paste0(imppath, 'SimulatedSIR.csv'))[-1, ]
-sim_dyn <- read.csv2(paste0(imppath, 'SimulatedSIR_dyn.csv'))[-1, ]
+#sim_const <- read.csv2(paste0(imppath, 'SimulatedSIR.csv'))[-1, ]
+#sim_dyn <- read.csv2(paste0(imppath, 'SimulatedSIR_dyn.csv'))[-1, ]
+
+actual_data <- read.csv2(paste0(imppath, 'EuropeCovidData.csv'))
+actual_data <- subset(actual_data, Country.Region == 'US')
+
+# drop first 80 observations (2M) to account for testing catching up
+actual_data <- tail(actual_data, -80)
+
 
 ###################################
 ########## Estimating R0 ##########
@@ -19,26 +26,34 @@ sim_dyn <- read.csv2(paste0(imppath, 'SimulatedSIR_dyn.csv'))[-1, ]
 
 # Austrian, US and French method
 
-params <- list(list(4.46, 2.63), list(5.2, 5.1), list(4, 4))
-names(params) <- c('Austria', 'USA', 'France')
+# params <- list(list(4.46, 2.63), list(5.2, 5.1), list(4, 4))
+# names(params) <- c('Austria', 'USA', 'France')
+#
+# full_eval <- list()
+#
+# for (i in 1:length(params)) {
+#   mean <- params[[i]][[1]]
+#   std <- params[[i]][[2]]
+#   est_r0 <- estimate_R(incid=as.numeric(actual_data$confirmed), method='parametric_si',
+#     config=make_config(list(mean_si=mean, std_si=std)))[[1]]
+#   est_r0 <- est_r0[c('Mean(R)', 'Std(R)', 't_end')]
+#   names(est_r0) <- c("R_Mean", 'R_Std', 'Days')
+#   eval <- left_join(sim_dyn, est_r0, by = 'Days')
+#   eval['lower'] <- eval['R_Mean'] - (1.96 * eval['R_Std'])
+#   eval['upper'] <- eval['R_Mean'] + (1.96 * eval['R_Std'])
+#   eval$Country <- rep(names(params)[[i]], dim(eval)[[1]])
+#   full_eval[[i]] <- eval
+# }
+#
+# df <- do.call(rbind, full_eval)
 
-full_eval <- list()
-
-for (i in 1:length(params)) {
-  mean <- params[[i]][[1]]
-  std <- params[[i]][[2]]
-  est_r0 <- estimate_R(incid=as.numeric(sim_dyn$Delta), method='parametric_si',
-    config=make_config(list(mean_si=mean, std_si=std)))[[1]]
-  est_r0 <- est_r0[c('Mean(R)', 'Std(R)', 't_end')]
-  names(est_r0) <- c("R_Mean", 'R_Std', 'Days')
-  eval <- left_join(sim_dyn, est_r0, by = 'Days')
-  eval['lower'] <- eval['R_Mean'] - (1.96 * eval['R_Std'])
-  eval['upper'] <- eval['R_Mean'] + (1.96 * eval['R_Std'])
-  eval$Country <- rep(names(params)[[i]], dim(eval)[[1]])
-  full_eval[[i]] <- eval
-}
-
-df <- do.call(rbind, full_eval)
+est_r0 <- estimate_R(incid=as.numeric(actual_data$confirmed), method='parametric_si',
+                     config=make_config(list(mean_si=5.2, std_si=5.1)))[[1]]
+est_r0 <- est_r0[c('Mean(R)', 'Std(R)', 't_end')]
+names(est_r0) <- c("R_Mean", 'R_Std', 'Days')
+est_r0['lower'] <- est_r0['R_Mean'] - (1.96 * est_r0['R_Std'])
+est_r0['upper'] <- est_r0['R_Mean'] + (1.96 * est_r0['R_Std'])
+est_r0$Method <- 'TSI'
 
 # German method
 
@@ -47,18 +62,15 @@ int_list <- list()
 intervals <- c(4, 5, 6, 7)
 for (inter in c(1, 2, 3, 4)) {
   interval <- intervals[inter]
-  r0_ger <- rep(NA, nrow(sim_dyn))
-  for (t in (3 + interval):nrow(sim_dyn)) {
-   r0_ger[t] <- sum(as.numeric(sim_dyn$Delta)[t-0:3]) / sum(as.numeric(sim_dyn$Delta)[t-interval:(3 + interval)])
+  r0_ger <- rep(NA, nrow(actual_data))
+  for (t in (3 + interval):nrow(actual_data)) {
+   r0_ger[t] <- sum(as.numeric(actual_data$confirmed)[t-0:3]) / sum(as.numeric(actual_data$confirmed)[t-interval:(3 + interval)])
   }
   r0_ger <- data.frame(r0_ger)
   # bind to evaluation dataframe
-  r0_ger <- cbind(sim_dyn, r0_ger)
+  r0_ger <- cbind(actual_data, r0_ger)
 
-  long <- reshape2::melt(r0_ger, id.vars = 'Days')
-  long['Interval'] <- rep(interval, dim(long)[1])
-
-  int_list[[inter]] <- long
+  int_list[[inter]] <- r0_ger
 }
 
 #ger <- do.call(rbind, int_list)
@@ -72,25 +84,25 @@ for (inter in c(1, 2, 3, 4)) {
 #while window_interval should 
 #represent the generation time, which we take to be 4
 
-r0_itafun <- function(ts, window_interval) {
-  data1 <- accelerometry::movingaves(ts, window=5)
-  names(data1) <- names(ts)[3:(length(ts)-2)]
-  res <- sapply((1+window_interval):length(data1), function(t) {
-    data1[t]/data1[t-window_interval]
-  })
-  return(res)
-}
-
-intervals <- c(4, 5, 6, 7)
-italist <- list()
-for (inter in c(1, 2, 3, 4)) {
-  interval <- intervals[inter]
-  r0_ita <- r0_itafun(as.numeric(sim_dyn$Delta), interval)
-  r0_ita <- data.frame(r0_ita) #Length of 92 since res only starts at 5th observation and MA window is +-2
-}
-r0_ita$Days = 12:299
-
-evalita <- left_join(sim_dyn, r0_ita[c('r0_ita', 'Days')], by = 'Days')
+# r0_itafun <- function(ts, window_interval) {
+#   data1 <- accelerometry::movingaves(ts, window=5)
+#   names(data1) <- names(ts)[3:(length(ts)-2)]
+#   res <- sapply((1+window_interval):length(data1), function(t) {
+#     data1[t]/data1[t-window_interval]
+#   })
+#   return(res)
+# }
+#
+# intervals <- c(4, 5, 6, 7)
+# italist <- list()
+# for (inter in c(1, 2, 3, 4)) {
+#   interval <- intervals[inter]
+#   r0_ita <- r0_itafun(as.numeric(sim_dyn$Delta), interval)
+#   r0_ita <- data.frame(r0_ita) #Length of 92 since res only starts at 5th observation and MA window is +-2
+# }
+# r0_ita$Days = 12:299
+#
+# evalita <- left_join(sim_dyn, r0_ita[c('r0_ita', 'Days')], by = 'Days')
 
 #ita <- do.call(rbind, italist)
 #ggplot(data = ita) +
@@ -102,30 +114,45 @@ evalita <- left_join(sim_dyn, r0_ita[c('r0_ita', 'Days')], by = 'Days')
 # names(r0_ger) <- c("dates", "R_sim", "R_Mean")
 colnames(r0_ger)[colnames(r0_ger) == 'r0_ger'] <- 'R_Mean'
 r0_ger['R_Std'] <- rep(0, dim(r0_ger)[1])
+r0_ger <- na.omit(r0_ger)
+r0_ger$Days <- 10:(nrow(est_r0)+7)
+r0_ger <- r0_ger[c('R_Mean', 'R_Std', 'Days')]
 r0_ger['lower'] <- rep(0, dim(r0_ger)[1])
 r0_ger['upper'] <- rep(0, dim(r0_ger)[1])
-r0_ger$Country <- rep("Germany", dim(r0_ger)[1])
-
-colnames(evalita)[colnames(evalita) == 'r0_ita'] <- 'R_Mean'
-evalita['R_Std'] <- rep(0, dim(evalita)[1])
-evalita['lower'] <- rep(0, dim(evalita)[1])
-evalita['upper'] <- rep(0, dim(evalita)[1])
-evalita$Country <- rep("Italy", dim(r0_ger)[1])
+r0_ger$Method <- 'Rolling Window'
 
 
-non_ci <- rbind(df, evalita)
-non_ci <- rbind(non_ci, r0_ger)
 
-simplot <- ggplot(data=non_ci) +  
-  geom_line(aes(x=Days, y=R_Mean, color='red')) +
-  geom_line(aes(x=Days, y=as.numeric(R0), col='blue')) +
-  geom_ribbon(aes(x=Days, ymin=lower, ymax=upper, fill='red'), alpha=0.3) +
-  facet_wrap(~Country, nrow=2, ncol=3, scale="free_y") +  ylab('Value of R') +  
-  scale_fill_identity(name = 'Confidence Interval', 
-    guide = 'legend',labels = c('CI')) +  
-  scale_colour_manual(name = 'R0 Type', 
-    values =c('red'='red','blue'='blue'), labels = c('Sim R0', 'Est R0')) +
-  ggsave(paste0(outpath, "All_Countries_withCI_r0_2.png"))
+# colnames(evalita)[colnames(evalita) == 'r0_ita'] <- 'R_Mean'
+# evalita['R_Std'] <- rep(0, dim(evalita)[1])
+# evalita['lower'] <- rep(0, dim(evalita)[1])
+# evalita['upper'] <- rep(0, dim(evalita)[1])
+# evalita$Country <- rep("Italy", dim(r0_ger)[1])
+
+
+# non_ci <- rbind(df, evalita)
+# non_ci <- rbind(non_ci, r0_ger)
+#
+# simplot <- ggplot(data=non_ci) +
+#   geom_line(aes(x=Days, y=R_Mean, color='red')) +
+#   geom_line(aes(x=Days, y=as.numeric(R0), col='blue')) +
+#   geom_ribbon(aes(x=Days, ymin=lower, ymax=upper, fill='red'), alpha=0.3) +
+#   facet_wrap(~Country, nrow=2, ncol=3, scale="free_y") +  ylab('Value of R') +
+#   scale_fill_identity(name = 'Confidence Interval',
+#     guide = 'legend',labels = c('CI')) +
+#   scale_colour_manual(name = 'R0 Type',
+#     values =c('red'='red','blue'='blue'), labels = c('Sim R0', 'Est R0')) +
+#   ggsave(paste0(outpath, "All_Countries_withCI_r0_2.png"))
+
+df <- rbind(est_r0, r0_ger)
+
+df %>% ggplot() +
+  geom_line(aes(x = Days, y = R_Mean, color = Method, group = Method)) +
+  geom_ribbon(aes(x=Days, ymin = lower, ymax = upper, group = Method)) +
+  ylab('Estimated R_0') +
+  theme_minimal() +
+  labs(caption = 'Data: US Daily Covid-19 Cases, Johns Hopkins University March 2020 - February 2021', color = '') +
+  ggsave(paste0(outpath, 'R0_Estimation.png'), width = 10, height = 5)
 
 
 
