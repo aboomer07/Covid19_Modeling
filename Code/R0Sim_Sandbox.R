f6f777f44cfb82aa2384e6bd5c29ddc7b8ecd0f5
jacob_314 <jacob.pichelmann@gmail.com>
2021-02-24 17:41:06 +0100

    Gamma experiments.

--- Code/R0Sim_Sandbox.R
+++ Code/R0Sim_Sandbox.R
@@ -64,7 +64,7 @@ experimentR(mean, std, R, window)
 
 days <- 100
 tau_m <-  11
-R <- 1.4
+R <- 1.6
 delta <- 1/24
 mean <- 6.6
 std <- 1.1
@@ -75,7 +75,7 @@ data <- data.frame(matrix(nrow = days*24, ncol = 4))
 colnames(data) <- c('t', 'days', 'R_t', 'infective')
 data$t <- 1:dim(data)[1]
 data$days <- rep(1:days, each = 24)
-data$infective[1:(tau_m*24)] <- 10
+data$infective[1:(tau_m*24)] <- 100
 data$R_t <- R
 
 # set up gamma
@@ -84,6 +84,32 @@ gamma_x <- seq(0, tau_m-1, 1)
 gamma_y <- rep(dgamma(gamma_x, mean, std), each=1/delta)
 gamma <- rev(gamma_y)
 
+# without any knowledge about R_t usually alpha = 1, beta = 5
+# https://www.pluralsight.com/guides/beta-and-gamma-function-implementation-in-r
+alpha <- 3
+beta <- 20
+mean <- alpha * beta
+std <- sqrt(alpha*beta^2)
+
+range <- seq(0, tau_m*24-1, 1)
+gamma_y <- dgamma(range, alpha, rate = 1/beta)
+plot(range, gamma_y, type ="l")
+gamma <- rev(gamma_y)
+
+# reconstruct from mean = 4.46, std = 2.63
+beta <- (2.63^2)/4.46
+alpha <- 4.46/beta
+
+alpha <- alpha
+beta <- beta
+
+# just to compare with Austrian estimation, looks very similar
+# https://www.ages.at/download/0/0/068cb5fb9f2256d267e1a3dc8d464623760fcc30/fileadmin/AGES2015/Wissen-Aktuell/COVID19/Sch%C3%A4tzung_des_seriellen_Intervalles_von_COVID19_2020-04-08.pdf
+range <- seq(0, tau_m+4, 1)
+gamma_og <- dgamma(range, alpha, rate = 1/beta)
+plot(range, gamma_og, type ="l")
+
+
 start <- ((tau_m) * 24) + 1
 
 for (t in start:dim(data)[1]) {
@@ -91,6 +117,10 @@ for (t in start:dim(data)[1]) {
 	R_mean <- mean(data[which(data$t==t),]$R_t)
 	total_infec <- sum(I_vec * gamma)
 
-	infec <- R_mean * 1/24 * total_infec
+	infec <- R_mean * total_infec
 	data[which(data$t == t),]$infective <- infec
 }
+
+I_vec <- data[data$t %in% (265-N):(265-1),]$infective
+R_mean <- mean(data[which(data$t==265),]$R_t)
+total_infec <- sum(I_vec * gamma)
