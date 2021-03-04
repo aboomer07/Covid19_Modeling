ba15c644016ff02db90ccfed7a385ec98350c2f3
jacob_314 <jacob.pichelmann@gmail.com>
2021-03-04 12:26:41 +0100

    Changed gamma and added confidence intervals.

--- Code/R0Sim_Nour.R
+++ Code/R0Sim_Nour.R
@@ -7,7 +7,6 @@ library(EpiEstim)
 library(R0)
 library(dplyr)
 library(ggplot2)
-library(accelerometry)
 library(tidyverse)
 
 imppath <- paste0(getwd(), '/Data/')
@@ -21,8 +20,7 @@ delta <- 1/24
 n <- window / delta
 
 
-
-nour_sim <- function(days = n_days, tau_m = window, R = R_val, N = n, plotname, noisy = F){
+nour_sim <- function(days = n_days, tau_m = window, R = R_val, N = n, plotname, noisy = F, conf = T){
 	# set up data
 	# set up data
 	data <- data.frame(matrix(nrow = days*24, ncol = 4))
@@ -37,14 +35,23 @@ nour_sim <- function(days = n_days, tau_m = window, R = R_val, N = n, plotname, 
 		data$R_t <- data$R_t + noise
 	}
 	# set up gamma
+	# andy:
 	# mean <- 94
 	# std <- 43.35897
 	# alpha <- (mean^2) / (std^2)
 	# beta <- mean / (std^2)
-	alpha <- 4.7
-	beta <- 20
-	mean <- alpha * beta
-	std <- sqrt(alpha*beta^2)
+
+	# jp random:
+	#alpha <- 4.7
+	#beta <- 20
+	#mean <- alpha * beta
+	#std <- sqrt(alpha*beta^2)
+
+	# jp new:
+	mean <- 6.6*24
+	std <- 1.1*24
+	beta <- std^2/mean
+	alpha <- mean/beta
 
 	print(alpha)
 	print(beta)
@@ -75,24 +82,56 @@ nour_sim <- function(days = n_days, tau_m = window, R = R_val, N = n, plotname, 
 	# estimate and export plot
 	mean <- mean*delta
 	std <- std*delta
-	tsi_est <- estimate_R(daily_infec$infective_day, method = 'parametric_si', config = make_config(list(mean_si = mean, std_si = std)))
-	jpeg(paste0(outpath, 'TSISim_Nour_', plotname, ".jpeg"))
-	par(mfrow=c(2,1), tcl=0.5, family="serif", mai=c(1,1,0.3,0.3))
-	plot(daily_infec$infective_day, type='l', xlab='Number of Days', ylab = 'Daily Infections')
-	plot(tsi_est$R$`Mean(R)`, type ="l", ylim = c(0,3), xlab='Number of Days', ylab='Rt')
-	lines(tail(daily_infec, -window)$R_val, col = "red")
-	legend(50, 1,
-	   legend = c("Estimated Rt", "Simulated Rt"),
-	   fill = c("black", "red"))
-	dev.off()
+	tsi_est <- estimate_R(daily_infec$infective_day, method = 'parametric_si', config = make_config(list(mean_si = mean,
+																										 std_si = std)))$R
+	tsi_est['lower'] <- tsi_est$`Mean(R)` - (1.96 * tsi_est$`Std(R)`)
+	tsi_est['upper'] <- tsi_est$`Mean(R)` + (1.96 * tsi_est$`Std(R)`)
+	newx <- 1:(days-7) # substract weekly window
+
+	if (conf){
+		jpeg(paste0(outpath, 'TSISim_Nour_', plotname, ".jpeg"))
+		par(mfrow=c(2,1), tcl=0.5, family="serif", mai=c(1,1,0.3,0.3))
+		plot(daily_infec$infective_day, type='l', xlab='Number of Days', ylab = 'Daily Infections')
+		plot(tsi_est$`Mean(R)`, type = 'n', ylim = c(0,3), xlab='Number of Days', ylab='Rt')
+		polygon(x = c(rev(newx), newx), y = c(rev(tsi_est[, 12]), tsi_est[, 13]), col = 'grey80', border = NA)
+		adjustcolor("grey80", alpha.f=0.05)
+		lines(tsi_est$`Mean(R)`)
+		lines(newx, tsi_est[, 12], lty = 'dashed', col = 'blue')
+		lines(newx, tsi_est[, 13], lty = 'dashed', col = 'blue')
+		lines(tail(daily_infec, -window)$R_val, col = "red")
+		legend(50, 1,
+			   legend = c("Estimated Rt", "Simulated Rt"),
+			   fill = c("black", "red"))
+		dev.off()
+	}
+	else {
+		jpeg(paste0(outpath, 'TSISim_Nour_', plotname, ".jpeg"))
+		par(mfrow=c(2,1), tcl=0.5, family="serif", mai=c(1,1,0.3,0.3))
+		plot(daily_infec$infective_day, type='l', xlab='Number of Days', ylab = 'Daily Infections')
+		plot(tsi_est$`Mean(R)`, type ="l", ylim = c(0,3), xlab='Number of Days', ylab='Rt')
+		lines(tail(daily_infec, -window)$R_val, col = "red")
+		legend(50, 1,
+			   legend = c("Estimated Rt", "Simulated Rt"),
+			   fill = c("black", "red"))
+		dev.off()
+	}
+
 }
 
-nour_sim(R = 1.3, plotname = 'ConstantR')
+nour_sim(R = 1.3, plotname = 'ConstantR', conf = F)
 
-nour_sim(R = 1.3, noisy = T, plotname = 'NoisyR')
+nour_sim(R = 1.3, noisy = T, plotname = 'NoisyR', conf = F)
 
-nour_sim(plotname = 'StepR')
+nour_sim(plotname = 'StepR', conf = F)
 
-nour_sim(noisy = T, plotname = 'StepR_noisy')
+nour_sim(noisy = T, plotname = 'StepR_noisy', conf = F)
 
+
+nour_sim(R = 1.3, plotname = 'ConstantR_CI')
+
+nour_sim(R = 1.3, noisy = T, plotname = 'NoisyR_CI')
+
+nour_sim(plotname = 'StepR_CI')
+
+nour_sim(noisy = T, plotname = 'StepR_noisy_CI')
 
