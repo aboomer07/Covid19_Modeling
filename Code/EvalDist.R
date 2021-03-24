4d04cb3e7469e1cb92c8fca4542a158df36403e8
nikiterjm <nikitamarini@gmail.com>
2021-03-24 23:07:46 +0100

    finished connecting all the functions; created plots for the EvalDist.R script

--- Code/EvalDist.R
+++ Code/EvalDist.R
@@ -25,32 +25,69 @@ dist_estimate <- function(study_len, sim_mean, sim_var, sim_type, vals){
 dist_estimate(study_len, sim_mean, sim_var, "weibull", vals)
 
 
-### Evaluate
+### Draw daily incidence of cases and corresponding Rt, given type of omega
 
-og_gamma <- gen_distribution(15, sim_mu, sim_var, "gamma")
-new_gamma <- gen_distribution(15, mean(params[params$Distribution == 'gamma', 'True_a']), mean(params[params$Distribution == 'gamma', 'True_b']),"gamma")
-og_weibull <- gen_distribution(15, a_weibull, b_weibull, "weibull")
-new_weibull<- gen_distribution(15, mean(params[params$Distribution == 'weibull', 'True_a']), mean(params[params$Distribution == 'weibull', 'True_b']),"gamma")
-og_norm <- gen_distribution(15, a_norm, b_norm, "norm")
-new_norm <- gen_distribution(15, mean(params[params$Distribution == 'norm', 'True_a']), mean(params[params$Distribution == 'norm', 'True_b']),"gamma")
-og_lnorm <- gen_distribution(15, a_lnorm, b_lnorm, "lnorm")
-new_lnorm <- gen_distribution(15, mean(params[params$Distribution == 'lnorm', 'True_a']), mean(params[params$Distribution == 'lnorm', 'True_b']),"gamma")
+infections_plot <- function(type){
+	samps <- samp_pois(R_val = 1.4, study_len = study_len, num_people = num_people,
+				   sim_mu = sim_mean, sim_sig = sim_var, sim_type = type, delta = delta)
+	vals <- serial_ests(samps)
+	incid <- nour_sim_data(sim_mean, sim_var, type, delta)
 
-jpeg("DistCompare.jpg")
-layout(matrix(1:4, nrow = 2, ncol=2))
-plot(og_gamma, type="l")
-lines(new_gamma, type="l", col="green")
-title("Gamma")
+	infected = incid$infective_day
+	Rt_sim = incid$R_val
 
-plot(og_weibull, type="l")
-lines(new_weibull, type="l", col="red")
-title("Weibull")
+	jpeg("IncidenceRt.jpg")
+	layout(matrix(1:2, ncol = 1, nrow =2))
+	plot(infected, type = "l", main = paste0("New infections per day (", type, ")"), xlab = "Day", ylab = "New infections")
+	plot(Rt_sim, type = "l", main = "Simulated R(t)", xlab = "Day", ylab = "Rt", ylim = c(0, 2))
+	dev.off()
+}
 
-plot(og_norm, type="l")
-lines(new_norm, type="l", col="blue")
-title("Normal")
+infections_plot(type = 'weibull')
 
-plot(og_lnorm, type="l")
-lines(new_lnorm, type="l", col="orange")
-title("Log-normal")
-dev.off()
+
+### Compare estimate of Rt to its true value 
+
+compare_rt <- function(simulate_dist, estimate_dist){
+
+	samps <- samp_pois(R_val = 1.4, study_len = study_len, num_people = num_people,
+				   sim_mu = sim_mean, sim_sig = sim_var, sim_type = simulate_dist, delta = delta)
+	vals <- serial_ests(samps)
+	incid <- nour_sim_data(sim_mean, sim_var, simulate_dist, delta)
+	Rt <- Rt_est(incid, vals, estimate_dist)
+	
+	jpeg("RtCompare.jpg")	
+	plot(Rt$Est_Rt, type = "l", main = paste0("R(t) estimation (", simulate_dist, ")"), xlab = "Day", ylab = "R(t)")
+	lines(Rt$Rt, col = "red")
+	dev.off()
+	}
+
+compare_rt(simulate_dist = 'weibull', estimate_dist = 'gamma')
+
+#og_gamma <- gen_distribution(15, sim_mu, sim_var, "gamma")
+#new_gamma <- gen_distribution(15, mean(params[params$Distribution == 'gamma', 'True_a']), mean(params[params$Distribution == 'gamma', 'True_b']),"gamma")
+#og_weibull <- gen_distribution(15, a_weibull, b_weibull, "weibull")
+#new_weibull<- gen_distribution(15, mean(params[params$Distribution == 'weibull', 'True_a']), mean(params[params$Distribution == 'weibull', 'True_b']),"gamma")
+#og_norm <- gen_distribution(15, a_norm, b_norm, "norm")
+#new_norm <- gen_distribution(15, mean(params[params$Distribution == 'norm', 'True_a']), mean(params[params$Distribution == 'norm', 'True_b']),"gamma")
+#og_lnorm <- gen_distribution(15, a_lnorm, b_lnorm, "lnorm")
+#new_lnorm <- gen_distribution(15, mean(params[params$Distribution == 'lnorm', 'True_a']), mean(params[params$Distribution == 'lnorm', 'True_b']),"gamma")
+#
+#jpeg("DistCompare.jpg")
+#layout(matrix(1:4, nrow = 2, ncol=2))
+#plot(og_gamma, type="l")
+#lines(new_gamma, type="l", col="green")
+#title("Gamma")
+#
+#plot(og_weibull, type="l")
+#lines(new_weibull, type="l", col="red")
+#title("Weibull")
+#
+#plot(og_norm, type="l")
+#lines(new_norm, type="l", col="blue")
+#title("Normal")
+#
+#plot(og_lnorm, type="l")
+#lines(new_lnorm, type="l", col="orange")
+#title("Log-normal")
+#dev.off()
