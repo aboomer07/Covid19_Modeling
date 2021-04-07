0eaed4c44f7cf2fea070b77881992255d06f0eab
Andy Boomer <aboomer07@gmail.com>
2021-04-07 10:53:25 -0700

    Adjusted functions

--- Code/Params.R
+++ Code/Params.R
@@ -6,7 +6,7 @@ params[['num_people']] <- 30
 params[['simulations']] <- 1000
 params[['sim_type']] <- 'gamma'
 params[['n_days']] <- 300
-params[['delta']] <- 24*4
+params[['delta']] <- 24
 params[['n']] <- params[['tau_m']] * params[['delta']]
 params[['R_val']] <- c(1.4, 1.1, 1.2)
 params[['pop']] <- 6000000
@@ -15,5 +15,5 @@ params[['sim_mu_variant']] <- 7
 params[['sim_var_variant']] <- 2
 params[['sim_type_variant']] <- 'gamma'
 params[['tau_m']] <- params[['study_len']]
-params[['start_variant']] <- 75
-params[['sii_cross']] <- 0.01
+params[['start_variant']] <- 50
+params[['sii_cross']] <- 0.1
