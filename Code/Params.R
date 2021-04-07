7db8ce55c37ffa689fb6284bae219af91a8c8441
Andy Boomer <aboomer07@gmail.com>
2021-04-06 22:33:38 -0700

    Adding parameters file

---
+++ Code/Params.R
@@ -0,0 +1,19 @@
+params <- list()
+params[['sim_mu']] <- 7
+params[['sim_var']] <- 2
+params[['study_len']] <- 20
+params[['num_people']] <- 30
+params[['simulations']] <- 1000
+params[['sim_type']] <- 'gamma'
+params[['n_days']] <- 300
+params[['delta']] <- 24
+params[['n']] <- params[['tau_m']] * params[['delta']]
+params[['R_val']] <- 1.5
+params[['pop']] <- 6000000
+params[['R_val_variant']] <- 1.9
+params[['sim_mu_variant']] <- 7
+params[['sim_var_variant']] <- 2
+params[['sim_type_variant']] <- 'gamma'
+params[['tau_m']] <- params[['study_len']]
+params[['start_variant']] <- 75
+params[['sii_cross']] <- 0.01
