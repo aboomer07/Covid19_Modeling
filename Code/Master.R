fb41465279ed3931dad06fe6509c1b00d2ab21cf
Andy Boomer <aboomer07@gmail.com>
2021-04-07 13:48:35 -0700

    Updated Plots

--- Code/Master.R
+++ Code/Master.R
@@ -76,23 +76,20 @@ plot_nonpara_eval(true_dist, nonpar_sim)
 ############## Simulate Incidence ####################################
 ######################################################################
 # simulate outbreak
-incid <- nour_sim_data(params) # important! must be same dist as in samp_pois
+# incid <- nour_sim_data(params) # important! must be same dist as in samp_pois
 
 incid <- si_sim(params)
-incid <- sii_sim(params)
-incid2 <- ssii_sim(params)
 
 # plot outbreak
 pdf(file = paste0(outpath, "Infections_", params[['sim_type']], ".pdf"))
 infections_plot(incid, params)
 dev.off()
 
-
 ######################################################################
 ##################### Estimate Rt ####################################
 ######################################################################
 
-Rt <- Rt_est(incid, vals, params, deterministic = T, correct_bias = T, variant = F)
+Rt <- Rt_est(incid, vals, params, deterministic = T, correct_bias = T, variant = T)
 
 Rt_nonpara <- Rt_est_nonpara(incid, samps, 'nsr', params)
 
@@ -101,87 +98,105 @@ png(file = paste0(outpath, "CompareRt_Variant", params[['sim_type']], ".png"))
 compare_rt(Rt, params, variant = F)
 dev.off()
 
-MSE <- MSE_est(Rt)
+######################################################################
+##################### SI Framework ###################################
+######################################################################
 
+params['R_val'] <- 0.9
+
+si_model <- si_sim(params)
+sii_model <- sii_sim(params)
+ssii_model <- ssii_sim(params)
+
+Rt_si <- Rt_est(si_model, vals, params, deterministic = T, correct_bias = T, variant = F)
+Rt_sii <- Rt_est(sii_model, vals, params, deterministic = T, correct_bias = T, variant = T, sep_Rt = T)
+Rt_ssii <- Rt_est(ssii_model, vals, params, deterministic = T, correct_bias = T, variant = T, sep_Rt = T, sep_S = T)
+
+si_plot(si_model, Rt_si)
+png(paste0(outpath, "/SII_Plot.png"))
+sii_plot(sii_model, Rt_sii)
+dev.off()
 
 ################################################################################
 #Plot the estimated R's vs. the Simulated R
 ################################################################################
 
-Rt_plot <- Rt %>%
-	group_by(est_a, est_b) %>%
-	mutate(id = cur_group_id()) %>%
-	group_by(Date) %>%
-	mutate(R_max = max(Est_Rt),
-	       R_min = min(Est_Rt))
+# MSE <- MSE_est(Rt)
 
-ggplot() +
-	geom_line(data = Rt_plot, aes(x = Date, y=Est_Rt, group=id), alpha=0.3) +
-	geom_line(data = Rt_plot, aes(x=Date, y=Rt), color = "red", size=1) +
-	scale_color_brewer(palette="Dark2") +
-	theme_minimal()
+# Rt_plot <- Rt %>%
+# 	group_by(est_a, est_b) %>%
+# 	mutate(id = cur_group_id()) %>%
+# 	group_by(Date) %>%
+# 	mutate(R_max = max(Est_Rt),
+# 	       R_min = min(Est_Rt))
 
-
-ggplot() +
-	geom_ribbon(data = Rt_plot, 
-		aes(x=Date, ymin = R_min, ymax = R_max), alpha=0.6) +
-	geom_line(data = Rt_plot, aes(x=Date, y=Rt), color = "red", size=1) +
-	scale_fill_brewer(palette="Dark2") +
-	theme_minimal() +
-	ggsave("Rt_Est.png")
-
-################################################################################
-#Plot the MSE vs. the Mean and Standard Dev
-################################################################################
-
-test <- MSE_mat
-
-test$dev_a <- (test$est_a - test$True_est_a) / test$True_est_a
-test$dev_b <- (test$est_b - test$True_est_b) / test$True_est_b
-
-test$b_group <-as.factor(test$dev_b)
-test$a_group <-as.factor(test$dev_a)
-
-testLine <- test %>%
-  group_by(est_type) %>%
-  summarize(True_est_a = mean(True_est_a), True_est_b = mean(True_est_b))
+# ggplot() +
+# 	geom_line(data = Rt_plot, aes(x = Date, y=Est_Rt, group=id), alpha=0.3) +
+# 	geom_line(data = Rt_plot, aes(x=Date, y=Rt), color = "red", size=1) +
+# 	scale_color_brewer(palette="Dark2") +
+# 	theme_minimal()
 
 
-a_gam <- ggplot(data=test[test$est_type == 'gamma',], aes(x=dev_a, y=MSE, group=b_group, color=b_group)) +
-	geom_line() + 
-	scale_color_brewer(palette="Dark2") +
-	ggsave("Gamma_MSE.png")
+# ggplot() +
+# 	geom_ribbon(data = Rt_plot, 
+# 		aes(x=Date, ymin = R_min, ymax = R_max), alpha=0.6) +
+# 	geom_line(data = Rt_plot, aes(x=Date, y=Rt), color = "red", size=1) +
+# 	scale_fill_brewer(palette="Dark2") +
+# 	theme_minimal() +
+# 	ggsave("Rt_Est.png")
 
-a_wei <- ggplot(data=test[test$est_type == 'weibull',], aes(x=dev_a, y=MSE, group=b_group, color=b_group)) +
-	geom_line() + 
-	scale_color_brewer(palette="Dark2") +
-	ggsave("Weibull_MSE.png")
+# ################################################################################
+# #Plot the MSE vs. the Mean and Standard Dev
+# ################################################################################
 
-a_norm <- ggplot(data=test[test$est_type == 'norm',], aes(x=dev_a, y=MSE, group=b_group, color=b_group)) +
-	geom_line() + 
-	scale_color_brewer(palette="Dark2") +
-	ggsave("Norm_MSE.png")
+# test <- MSE_mat
 
-a_lnorm <- ggplot(data=test[test$est_type == 'lnorm',], aes(x=dev_a, y=MSE, group=b_group, color=b_group)) +
-	geom_line() + 
-	scale_color_brewer(palette="Dark2") +
-	ggsave("LNorm_MSE.png")
+# test$dev_a <- (test$est_a - test$True_est_a) / test$True_est_a
+# test$dev_b <- (test$est_b - test$True_est_b) / test$True_est_b
 
-est_b <- 
-  # ggplot(data=test, aes(x=est_a, y=MSE)) +
-  ggplot(data=test[test$est_type == 'gamma',], aes(x=dev_a, y=MSE, group=b_group, color=b_group)) +
-  geom_line()+ 
-  # facet_grid(~ est_type, scales='free')+ 
-  # geom_vline(data=testLine[testLine$est_type == 'gamma', ], aes(xintercept = True_est_a), linetype="dotted") +
-  scale_color_brewer(palette="Dark2")
-est_b
+# test$b_group <-as.factor(test$dev_b)
+# test$a_group <-as.factor(test$dev_a)
 
-est_a <- test %>%
-  ggplot( aes(x=est_b, y=MSE, group=est_a, color=est_a)) +
-  geom_line()+ 
-  facet_grid(~ est_type)+ 
-  geom_vline(data=testLine, aes(xintercept=est_b))+
-  scale_color_brewer(palette="Dark2")
-est_a
+# testLine <- test %>%
+#   group_by(est_type) %>%
+#   summarize(True_est_a = mean(True_est_a), True_est_b = mean(True_est_b))
+
+
+# a_gam <- ggplot(data=test[test$est_type == 'gamma',], aes(x=dev_a, y=MSE, group=b_group, color=b_group)) +
+# 	geom_line() + 
+# 	scale_color_brewer(palette="Dark2") +
+# 	ggsave("Gamma_MSE.png")
+
+# a_wei <- ggplot(data=test[test$est_type == 'weibull',], aes(x=dev_a, y=MSE, group=b_group, color=b_group)) +
+# 	geom_line() + 
+# 	scale_color_brewer(palette="Dark2") +
+# 	ggsave("Weibull_MSE.png")
+
+# a_norm <- ggplot(data=test[test$est_type == 'norm',], aes(x=dev_a, y=MSE, group=b_group, color=b_group)) +
+# 	geom_line() + 
+# 	scale_color_brewer(palette="Dark2") +
+# 	ggsave("Norm_MSE.png")
+
+# a_lnorm <- ggplot(data=test[test$est_type == 'lnorm',], aes(x=dev_a, y=MSE, group=b_group, color=b_group)) +
+# 	geom_line() + 
+# 	scale_color_brewer(palette="Dark2") +
+# 	ggsave("LNorm_MSE.png")
+
+# est_b <- 
+#   # ggplot(data=test, aes(x=est_a, y=MSE)) +
+#   ggplot(data=test[test$est_type == 'gamma',], aes(x=dev_a, y=MSE, group=b_group, color=b_group)) +
+#   geom_line()+ 
+#   # facet_grid(~ est_type, scales='free')+ 
+#   # geom_vline(data=testLine[testLine$est_type == 'gamma', ], aes(xintercept = True_est_a), linetype="dotted") +
+#   scale_color_brewer(palette="Dark2")
+# est_b
+
+# est_a <- test %>%
+#   ggplot( aes(x=est_b, y=MSE, group=est_a, color=est_a)) +
+#   geom_line()+ 
+#   facet_grid(~ est_type)+ 
+#   geom_vline(data=testLine, aes(xintercept=est_b))+
+#   scale_color_brewer(palette="Dark2")
+# est_a
 
 
